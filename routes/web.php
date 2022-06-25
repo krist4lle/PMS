6a4fde07947a58eacc2248ee8982a7aed1ff9cfa
Oleksandr Kuznietsov <alex.krist4lle@gmail.com>
2022-06-25 20:41:24 +0300

    feat: mailing done

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/home', App\Http\Controllers\HomeController::class)->name('index');
 
     Route::get('/employees', [App\Http\Controllers\Employee\IndexController::class, 'index'])
